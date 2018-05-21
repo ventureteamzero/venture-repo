d8200745e6cd98eac86c16495ffc2c8e87cd0d53
Kristine Concha <kristineconcha@kristines-macbook-pro.local>
2018-05-21 15:36:31 +0400

    Initial Commit

---
+++ testapp/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  testapp
+//
+//  Created by Kristine Concha on 5/21/18.
+//  Copyright © 2018 VENTURE GENERAL TRADING. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
